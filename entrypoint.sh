a6908278542378d2eea64bbf28d151026cec517f
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-08-14 15:49:23 -0700

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,6 +40,7 @@ lddtool_version=$(curl --silent "https://api.github.com/repos/NASA-PDS/pds4-info
 im_version=$(curl --silent "https://raw.githubusercontent.com/NASA-PDS/pds4-information-model/v${lddtool_version}/model-ontology/src/ontology/Data/config.properties" | grep 'infoModelVersionId' | awk -F= '{print $NF}')
 
 echo foo
+ls /usr/local/bin
 python3 --version
 python --version
 echo foo
