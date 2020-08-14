48809ac1b04f1d532f9a1c45b9b1bf071a8bd308
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-08-14 14:44:43 -0700

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,6 +57,7 @@ tar -xf /tmp/lddtool-${lddtool_version}-bin.tar.gz -C /tmp/
 # Generate Dictionaries
 log_info " Cleanup development versions if they exist"
 echo "dirpath: $dirpath"
+echo dirname $dirpath
 parent_dir=$(dirname $dirpath)
 echo $parent_dir
 rm -fr $parent_dir
