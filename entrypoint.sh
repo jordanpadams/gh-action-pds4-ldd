7bccaaa4834fb3d47a5c5321a3502a8b9bf23b47
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-08-17 15:48:16 -0700

    Action updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,7 +72,9 @@ fi
 java_cmd=$(which java)
 echo "java_cmd $java_cmd"
 parent_dir=$(dirname $java_cmd)
-echo "java home $parent_dir"
+echo "parent dir $parent_dir"
+java_home=$(dirname $parent_dir)
+echo "java home $java_home"
 
 log_info "Generating dictionaries for $files"
 /tmp/lddtool-$lddtool_version/bin/lddtool -plJn $files
