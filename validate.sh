a3c0885557c8b96a738c2b3b978fe0e285c35104
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-08-17 18:39:48 -0700

    Action updates

--- validate.sh
+++ validate.sh
@@ -10,21 +10,11 @@ log_error () {
 datapath="$1"
 schemas="$2"
 schematrons="$3"
-skip_content_validation="$4"
-failure_expected="$5"
-
-# Check valid datapath is specified
-if [ -z "$datapath" ]; then
-    log_error "Valid directory path must be specified (datapath)."
-fi
-
-if  [ ! -d "$datapath" ]; then
-    log_error "Valid directory path must be specified. Datapath: $datapath"
-fi
+failure_expected="$4"
 
 # Check datapath contains schemas / schematrons / labels to validate
-if ! ls $datapath/*.xml 1> /dev/null 2>&1 ; then
-    log_error "Invalid datapath. Must contain at least one of schema, schematron, and XML label"
+if ! ls $datapath 1> /dev/null 2>&1 ; then
+    log_error "Invalid datapath. Must contain at least one XML label"
 fi
 
 # Get latest validate version from Github
@@ -48,16 +38,13 @@ if [ -n "$schematrons" ]; then
 fi
 
 # Validate the data
-/tmp/validate-${validate_version}/bin/validate -t $datapath/*_VALID.xml $args
-valid_exitcode=$?
-
-/tmp/validate-${validate_version}/bin/validate -t $datapath/*_FAIL.xml $args
-fail_exitcode=$?
+/tmp/validate-${validate_version}/bin/validate -t $datapath $args
+exitcode=$?
 
 # Check for expected failure
-if $fail_exitcode && [ "$failure_expected" == "true" ]; then
+if $exitcode && [ "$failure_expected" == "true" ]; then
     echo "[FAIL] Validate expected to fail, but executed successfully."
     exit 1
 fi
 
-exit $valid_exitcode
+exit $exitcode
